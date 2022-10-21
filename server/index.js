4c42186aae7b36419e9e11990a9bae11a5153e80
Mashequr Khan <khanmashequr@gmail.com>
2022-10-21 16:39:16 +0600

    blockchain

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
+const mysql = require('mysql')
 
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'blockchain_database'
+});
 
 
 app.get("/", (req, res)=> {
-    res.send("hello Mashequr");
+    const sqlInsert = "INSERT INTO blockchain (id, hash, nonce, block_data, prev_hash, user_id) VALUES ('123','abc#123', '123*abc', 'stuff', '12#ab', 'mash');"
+    db.query(sqlInsert, (err, result)=> {
+        res.send("hello Mashequr");
+    });  
 });
 
 app.listen(3001, () => {
